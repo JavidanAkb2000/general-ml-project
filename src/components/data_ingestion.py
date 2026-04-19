2aba2b8c0833859242bdeb7bb1e6a764985669b4
JavidanAkb2000 <akberovcavidan1@gmail.com>
2026-04-20 00:09:51 +0400

    model_trainer.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,5 +1,7 @@
 import os
 import sys
+
+from src.components.model_trainer import ModelTrainer
 from src.logger import logging
 from src.exceptions import CustomException
 
@@ -55,4 +57,6 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _= data_transformation.initiate_data_transformation(train_data, test_data)
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
